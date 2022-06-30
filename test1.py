8d5c5eb37edd6e4156f341ccb898ae912216b819
jxliu <liujixiang@126.com>
2022-06-30 16:00:14 +0800

    first commit

---
+++ test1.py
@@ -0,0 +1,14 @@
+
+import pandas as pd
+
+import time
+import matplotlib.pyplot as plt
+
+f, ax = plt.subplots(2, 2)
+# 设置主标题
+f.suptitle('My Figure')
+# 设置子标题
+name2 = 'here'
+name = 'Test'+name2
+ax[0][0].set_title('sdds',name)
+plt.show()
